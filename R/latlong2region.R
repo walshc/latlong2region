949d0ae2fff475f9294bc89cdbd378d1c4a8e548
Christoph Walsh <criostoirbreathnach@gmail.com>
2016-09-03 15:39:27 -0400

    first commit

---
+++ R/latlong2region.R
@@ -0,0 +1,50 @@
+latlong2region <- function(p, shp, closest = TRUE) {
+
+  shp <- spTransform(shp, CRS("+proj=longlat +datum=WGS84"))
+
+  if (class(p) != "SpatialPoints") {
+    if (class(p) == "numeric") {
+      p <- matrix(p, ncol = 2)
+    }
+    p <- SpatialPoints(p, proj4string = CRS("+proj=longlat +datum=WGS84"))
+  }
+  df <- cbind(coordinates(p), sp::over(x = p, y = shp))
+  names(df)[1:2] <- c("lon", "lat")
+
+
+  if (closest) {
+    ids <- 1:nrow(df)
+    shp@data$latlong2region.id <- 1:nrow(shp@data)
+
+    # Get the IDs of the unmatched observations
+    unmatched <- ids[is.na(df[[names(shp@data)[1]]])]
+
+    df$used.closest <- is.na(df[[names(shp@data)[1]]])
+
+    if (length(unmatched) > 0) {
+      message(length(unmatched), " point(s) not inside any region.",
+              " Assigning to closest region.")
+      # Convert the shapefile into a data.frame
+      require(maptools)
+      shp.df <- ggplot2::fortify(shp, region = "latlong2region.id")
+    }
+
+    for (i in unmatched) {
+      # Get the coordinates of the unmatched points:
+      crds <- c(unique(df[[1]][ids == i]), unique(df[[2]][ids == i]))
+
+      # Find the distance between the point and every point in the polygon file:
+      distance <- geosphere::distHaversine(crds, cbind(shp.df$long, shp.df$lat))
+
+      # For the closest point, get the county ID
+      closest <- shp.df$id[which.min(distance)]
+
+      # Assign the county name corresponding to this ID to your missing data:
+      for (j in names(shp@data)[-ncol(shp@data)]) {
+        df[[j]][ids == i] <-
+          shp@data[[j]][shp@data$latlong2region.id == closest]
+      }
+    }
+  }
+  return(df)
+}
---
+++ README.md
@@ -0,0 +1,43 @@
+# latlong2region
+An `R` package to match coordinates to regions in a shapefile. Also includes
+the possibility to find the region a point is closest to if, for example, the
+point is the water.
+
+## Installation
+
+```r
+if (!require(devtools)) install.packages("devtools")
+devtools::install_github("walshc/latlong2region")
+```
+
+## Example Usage
+Download, extract and load a shapefile of US counties:
+```r
+download.file("http://www2.census.gov/geo/tiger/GENZ2015/shp/cb_2015_us_county_500k.zip", "us_counties.zip")
+unzip("us_counties.zip")
+shp <- rgdal::readOGR(".", "cb_2015_us_county_500k")
+```
+Create a `data.frame` of coordinates. Here, the coordinates for Chicago are in Lake Michigan so they won't be matched to any county.
+```r
+df <- data.frame(name = c("New York", "San Francisco", "Chicago"),
+                 lon  = c(-74.00594, -122.41942, -87.61),
+                 lat  = c(40.71278, 37.77493, 41.87811))
+```
+The function will find the county the point is closest to:
+
+```r
+p <- cbind(df$lon, df$lat)
+latlong2region(p, shp)
+```
+
+### Output
+```
+         lon      lat STATEFP COUNTYFP COUNTYNS       AFFGEOID GEOID          NAME LSAD
+1  -74.00594 40.71278      36      061 00974129 0500000US36061 36061      New York   06
+2 -122.41942 37.77493      06      075 00277302 0500000US06075 06075 San Francisco   06
+3  -87.61000 41.87811      17      031 01784766 0500000US17031 17031          Cook   06
+       ALAND     AWATER used.closest
+1   58678310   28554415        FALSE
+2  121455687  479135404        FALSE
+3 2448400204 1785626819         TRUE
+```
